8daa382632d9ce197db6791d431ae815ba43555f
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-03-12 15:26:02 -0300

    up

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,38 +1,76 @@
-import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+// button and input
+// import React from 'react';
+// import { render, fireEvent, cleanup } from '@testing-library/react';
 
+// import TechList from '~/components/TechList';
+
+// describe('TechList component', () => {
+//   beforeEach(() => {
+//     localStorage.clear();
+//   })
+
+//   it('should be able to add new tech', () => {
+//     const { getByText, getByTestId, debug, getByLabelText } = render(<TechList/>);
+
+//     // debug();
+//     // fireEvent.click(getByText('Adicionar'));
+//     // debug();
+
+//     fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+//     fireEvent.submit(getByTestId('tech-form'));
+
+//     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+//     expect(getByLabelText('Tech')).toHaveValue('');
+//   });
+
+//   it('should store techs in storage', () => {
+//     let { getByTestId, getByLabelText, getByText } = render(<TechList />)
+
+//     fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+//     fireEvent.submit(getByTestId('tech-form'));
+
+//     cleanup();
+
+//     ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
+
+//     expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']))
+//     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+//   })
+// });
+
+
+// redux
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+
+import { addTech } from '~/store/modules/techs/actions';
 import TechList from '~/components/TechList';
 
+jest.mock('react-redux');
+
 describe('TechList component', () => {
-  beforeEach(() => {
-    localStorage.clear();
-  })
+  it('should render tech list', () => {
+    useSelector.mockImplementation(cb => cb({
+      techs: ['Node.js', 'ReactJS']
+    }));
 
-  it('should be able to add new tech', () => {
-    const { getByText, getByTestId, debug, getByLabelText } = render(<TechList/>);
-
-    // debug();
-    // fireEvent.click(getByText('Adicionar'));
-    // debug();
-
-    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
-    fireEvent.submit(getByTestId('tech-form'));
-
+    const{ getByTestId, getByText } = render(<TechList />);
+    
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
-    expect(getByLabelText('Tech')).toHaveValue('');
+    expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
   });
 
-  it('should store techs in storage', () => {
-    let { getByTestId, getByLabelText, getByText } = render(<TechList />)
+  it('should be able to add new tech', () => {
+    const { getByTestId, getByLabelText } = render(<TechList />);
+
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
 
     fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
     fireEvent.submit(getByTestId('tech-form'));
 
-    cleanup();
-
-    ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
-
-    expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']))
-    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
-  })
+    expect(dispatch).toHaveBeenCalledWith(addTech('Node.js'));
+  });
 });
--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,3 +1,4 @@
+// testando button
 // import React, { useState } from 'react';
 
 // export default function TechList() {
@@ -16,26 +17,62 @@
 //     </div>
 //   );
 // }
-import React, { useState, useEffect } from 'react';
+
+
+// testando input
+// import React, { useState, useEffect } from 'react';
+
+// export default function TechList() {
+//   const [techs, setTechs] = useState([]);
+//   const [newTech, setNewTech] = useState('');
+
+//   useEffect(() => {
+//     const techs = localStorage.getItem('techs');
+
+//     if (techs) {
+//       setTechs(JSON.parse(techs));
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     localStorage.setItem('techs', JSON.stringify(techs));
+//   }, [techs]);
+
+//   function handleAddTech() {
+//     setTechs([...techs, newTech]);
+//     setNewTech('');
+//   }
+
+//   return (
+//     <form data-testid="tech-form" onSubmit={handleAddTech}>
+//       <ul data-testid="tech-list">
+//         {techs.map(tech => <li key={tech}>{tech}</li>)}
+//       </ul>
+
+//       <label htmlFor="tech">Tech</label>
+//       <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)}/>
+      
+//       <button onClick={handleAddTech}>Adicionar</button>
+//     </form>
+//   );
+// }
+
+
+// testando redux
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { addTech } from '../../store/modules/techs/actions';
 
 export default function TechList() {
-  const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
 
-  useEffect(() => {
-    const techs = localStorage.getItem('techs');
-
-    if (techs) {
-      setTechs(JSON.parse(techs));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('techs', JSON.stringify(techs));
-  }, [techs]);
+  const dispatch = useDispatch();
+  const techs = useSelector(state => state.techs);
 
   function handleAddTech() {
-    setTechs([...techs, newTech]);
+    dispatch(addTech(newTech));
+
     setNewTech('');
   }
 
